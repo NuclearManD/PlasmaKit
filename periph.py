427543bb8d7b1fcc630916eca0d48462fc559ea5
NuclearManD <dylanbrophy@gmail.com>
2019-08-01 00:33:42 -0700

    YASSS I MADE PERIPHERALS ABLE TO ADD FUNCTIONS DYNAMICALLY

--- periph.py
+++ periph.py
@@ -4,7 +4,7 @@ class PeripheralRemote:
         self.name = name
         self.type = t
     def __add_method__(self, name):
-        exec("self."+name+" = lambda *a: self.__call_remote__('"+name+"',a)", None, {'self':self})
+        setattr(self, name, eval("lambda self,*a: self.__call_remote__('"+name+"',a)", {}, {}).__get__(self))
     def __call_remote__(self, fn, args):
         print(fn+":", *args)
         
