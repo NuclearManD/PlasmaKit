fc809991eb76c0ad84f3634327d022446b1c5cda
NuclearManD <dylanbrophy@gmail.com>
2019-08-06 23:48:52 -0700

    work!!!

--- system.py
+++ system.py
@@ -1,4 +1,5 @@
-import periph
+import periph, _thread
+net = periph.net
 from random import randint
 
 def gen_adr():
@@ -6,24 +7,63 @@ def gen_adr():
     area_code = area_code_base | randint(0,0xFFFFFF)
     return (area_code<<16)|0x0001
 
-default_peers = []
+csys = None
+server_started = False
+def server_code(netkey, sys):
+    global server_started
+    open_port = net.NrlOpenPort(0x192291, 0x291192)
+    server_started = True
+    while True:
+        kleg = open_port.recv()
+        if kleg!=None:
+            src = kleg[0]
+            data = kleg[1].split(b'\x00')
+            cmd = data[0]
+            if cmd==b'getkey':
+                open_port.send(src,netkey.encode())
+            elif cmd==b'has_cpu':
+                if sys.local_cpu!=None:
+                    res = b'1'
+                else:
+                    res = b'0'
+                open_port.send(src, res)
+def start_server(address, netkey, sys):
+    periph.start_server(address)
+    print("Starting system server...")
+    _thread.start_new_thread(server_code,(netkey,sys))
 
+default_peers = []
 class System:
-    def __init__(self, peers=default_peers, address = gen_adr(), use_cpu = True, dbg = print):
-        periph.start_server(address)
+    def __init__(self, peers=default_peers, address = gen_adr(), netkey = 'comnet', use_cpu = True, dbg = print):
+        global csys
+        if csys!=None:
+            raise Exception("System already exists.")
         if use_cpu:
             self.local_cpu = periph.PeriphCPU()
+            periph.bindLocalPeripheral(self.local_cpu, 'sys_cpu')
         else:
             self.local_cpu = None
 
+        self.netkey = netkey
+        
+        start_server(address, netkey, self)
+        csys = self
+
         if(dbg!=None):
             dbg("Connecting to peers...")
-
+        self.peers = {}
+            
         for i in peers:
             try:
-                periph = periph.lsperiph(i)
+                if self.test_peer(i):
+                    self.peers[i] = True
             except:
                 if(dbg!=None):
                     dbg(hex(i)+" is not online.")
         
-        self.global_cpu = periph.PeriphCombinedCPU(cpus)
+        #self.global_cpu = periph.PeriphCombinedCPU(cpus)
+    def test_peer(self, adr):
+        con = net.NrlConnection(adr, 0x291192, 0x192291)
+        con.send(b'getkey')
+        return con.recv(8000).decode()==self.netkey
+            
